2744b521d3f5795dbc86ec03211c477d78cdb60b
kathyslate <mercyoyetunde98@gmail.com>
2024-01-04 13:38:03 -0800

    009988

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,9 +6,9 @@ a = 10
 b = 5
 
 result1 = calculator_1.add(a, b)
-result2 = calculator_1.subtract(a, b)
-result3 = calculator_1.multiply(a, b)
-result4 = calculator_1.divide(a, b)
+result2 = calculator_1.sub(a, b)
+result3 = calculator_1.mul(a, b)
+result4 = calculator_1.div(a, b)
 
 print(f"Addition of {a} and {b} is: {result1}")
 print(f"Subtraction of {a} and {b} is: {result2}")
