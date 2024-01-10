a57bf36435feed8faec0aa11e9241cd37b8caea6
kathyslate <mercyoyetunde98@gmail.com>
2024-01-10 05:47:38 -0800

    0o0onn

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def complex_(a_d, value):
- for key in list(a_dictionary.keys()):
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary):
         if a_dictionary[key] == value:
             del a_dictionary[key]
+    return a_dictionary
