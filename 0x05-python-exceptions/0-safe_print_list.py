7a1dc59b07fc1535cbbe67e895493b46a3304810
kathyslate <mercyoyetunde98@gmail.com>
2024-01-22 13:14:22 -0800

    oooookkkk

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,15 +4,13 @@ def safe_print_list(my_list=[], x=0):
     error_element = None
 
     try:
-        for element in my_list:
-            if print_count < x:
-                print(element, end=' ')
-                print_count += 1
-            else:
-                break
+        for element in my_list[:x]:
+            print(element, end='')
+            print_count += 1
     except TypeError:
+        error_element = my_list[0] if my_list else None
         print("Error: List contains non-printable type '{}'.".format(error_element))
-        return 0
+        return print_count
 
     print() # newline after all elements are printed
     return print_count
