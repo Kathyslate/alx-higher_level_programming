f69ecd6ef7036a6364415ccdf28d97d241fd976b
kathyslate <mercyoyetunde98@gmail.com>
2024-01-22 14:54:02 -0800

    juju

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,6 +2,9 @@
 def safe_print_division(a, b):
     try:
         division_result = a / b if b else None
+    except TypeError:
+        print("Error: Both operands must be integers or floats")
+        return None
     finally:
         print("Inside result: {}".format(division_result))
         return division_result
