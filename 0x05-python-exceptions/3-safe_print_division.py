9ae1d5b5afabf89ad150b5ee4ff5c2735e5140df
kathyslate <mercyoyetunde98@gmail.com>
2024-01-22 14:50:40 -0800

    gggtt

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        division_result = a / b if b else None
+    finally:
+        print("Inside result: {}".format(division_result))
+        return division_result
