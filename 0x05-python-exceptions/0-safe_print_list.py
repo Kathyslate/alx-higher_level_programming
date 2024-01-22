ad5b7582454a82b6b9aecb2a01316289b156495a
kathyslate <mercyoyetunde98@gmail.com>
2024-01-22 12:25:36 -0800

    bout now

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if x < 0:
+        raise ValueError("x should be greater than or equal to 0")
+    real_x = 0
     try:
-        count = 0
         for i in range(x):
-            if i < len(my_list):
-                print(my_list[i], end=' ')
-                count += 1
-            elif i >= len(my_list) and my_list:
-                print(my_list[-1], end=' ')
-                count += 1
-        print()
-        return count
-    except (TypeError, IndexError):
-        print("Error: List must contain only printable elements")
-        return 0
+            print(my_list[i], end=' ')
+            real_x += 1
+    except IndexError:
+        pass
+    print()
+    return real_x
