b448946efd1cdbcb7f04f48b2418893edd7d5d7b
kathyslate <mercyoyetunde98@gmail.com>
2024-01-22 12:51:28 -0800

    kkkkkk

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    if x < 0:
-        raise ValueError("x should be greater than or equal to 0")
-
+def safe_print_list(my_=[], x=0):
     real_x = 0
     for i in range(x):
         try:
-            print(my_list[i], end=' ')
+            print(my_list[i], end='')
             real_x += 1
         except (IndexError, TypeError):
             break
