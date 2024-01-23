0b4502aebcc4cd6c4c3de599cb38b8eade4897cb
kathyslate <mercyoyetunde98@gmail.com>
2024-01-22 16:54:13 -0800

    rghm

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+    except (ValueError, TypeError) as val:
+        print("Exception: {}".format(val), file=sys.stderr)
         return False
