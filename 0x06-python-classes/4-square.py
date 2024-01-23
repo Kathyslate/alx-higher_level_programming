65d87a664b59994dade95cb8b5cbf560960fd9cb
kathyslate <mercyoyetunde98@gmail.com>
2024-01-23 14:07:17 -0800

    Wtrey

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+
+class Square:
+    """ class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """Creates new instances of square.
+
+        Args:
+            size: size of the square (1 side).
+        """
+        self.__size = size
+
+    @size.setter
+    def size(self, value):
+        """Property setter for size.
+
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size  #: size of the square
+
+    def area(self):
+        """returns the area.
+
+        Returns:
+            ares.
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Returns the size of a square
+        """
+        return self.__size
+~                                        
