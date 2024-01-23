aef381fe190b0a596fbe426411b0af03659c77f8
kathyslate <mercyoyetunde98@gmail.com>
2024-01-22 17:10:15 -0800

    popo

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,8 @@ import sys
 def safe_function(fct, *args):
     try:
         rest = fct(*args)
-    except BaseException as val:
-        rest =  None
-        print("Exception: {}".format(val), file=sys.stderr)
-    return rest
+    except BaseException as e:
+        rest = None
+        print("Exception: {}".format(e), file=sys.stderr)
+    finally:
+        return rest
