048f6ffcb428b79578496bf3c3853f71d0ea6ad7
Kathyslate <mercyoyetunde98@gmail.com>
2024-01-31 04:04:09 -0800

    wsws

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -10,8 +10,8 @@ class LockedClass:
     Attributes:
         first_name (str): a person's first name.
     """
-    def __setattr__(self, key, value):
+
+    __slots__ = ["first_name"]
+    def __init__(self):
     """initiates instances for locked class"""
-        if key != "first_name" and not hasattr(self, key):
-            raise AttributeError("Cannot create new instance attribute")
-        super().__setattr__(key, value)
+         self.first_name = "first_name"
