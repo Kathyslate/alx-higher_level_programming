d0c1c2209fecceaab99a9ce9365f5457668dd0ff
Kathyslate <mercyoyetunde98@gmail.com>
2024-01-31 04:34:40 -0800

    works now

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -26,7 +26,6 @@ def add_integer(a, b=98):
 
     return (a + b)
 
-
     if __name__ == "__main__":
-       	import doctest
+        import doctest
         doctest.testfile("tests/0-add_integer.txt")
--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -8,14 +8,14 @@ import numpy
 def lazy_matrix_mul(m_a, m_b):
     """
     Multiplies two matrices using the NumPy module.
-    
+
     Parameters:
     m_a (list of lists or 2D NumPy array): First matrix.
     m_b (list of lists or 2D NumPy array): Second matrix.
-    
+
     Returns:
     2D NumPy array: Result of the multiplication.
-    
+
     Raises:
     LazyMatrixMulError: If the matrices cannot be multiplied.
     """
