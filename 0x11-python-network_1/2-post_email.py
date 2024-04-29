1ff8bb3c9f1939e4344e8dddbacfab4e3b7063cc
Kathyslate <mercyoyetunde98@gmail.com>
2024-04-29 15:06:13 +0100

    fghjk

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" takes in a URL and an email, sends a POST request to the passed URL with the email"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    param = {
+        "email": email
+    }
+    query_string = urllib.parse.urlencode(param)
+    data = query_string.encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        response_text = response.read().decode("utf-8")
+        print(response_text)
