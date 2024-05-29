bcd4954a09e24acef534e75ff439f472892c43bf
Kathyslate <mercyoyetunde98@gmail.com>
2024-05-29 03:41:04 -0700

    hghgfdfdsads

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Your script must be imported from the <head> tag,
+
+document.addEventListener('DOMContentLoaded', function () {
+  const headerElem = document.querySelector('HEADER');
+  headerElem.style.color = '#FF0000';
+});
